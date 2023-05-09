83f37c0b925e780d9c9726043f6d6a40c2e31ccb
HoudaifaBouamine <119418687+HoudaifaBouamine@users.noreply.github.com>
2023-05-09 23:17:45 +0100

    Create Functions : is_binary_search_tree(r : ptrTree) : boolean; / get_min(r : ptrTree) : integer; / get_max(r : ptrTree) : integer

--- tree.pas
+++ tree.pas
@@ -74,8 +74,54 @@ begin
 	
 end;
 
+function max(num1 ,num2: integer) : integer;
+begin
+	if(num1 > num2) then max := num1 else max := num2; 
+end;
+
+function min(num1 ,num2: integer) : integer;
+begin
+	if(num1 < num2) then min := num1 else min := num2; 
+end;
+
+function get_max(root : ptrTree) : integer;
+begin
+	
+	if(root^.left = nil) and (root^.right = nil) then
+        get_max := root^.value
+    else if(root^.left = nil) then // so right is not nil
+        get_max := max(get_max(root^.right),root^.value)
+    else if(root^.right = nil) then // so left is not nil
+        get_max := max(get_max(root^.left),root^.value)
+    else // both are not nil
+        get_max := max(max(get_max(root^.left),root^.value),get_max(root^.right));
+	
+end;
+
+function get_min(root : ptrTree) : integer;
+begin
+	
+	if(root^.left = nil) and (root^.right = nil) then
+        get_min := root^.value
+    else if(root^.left = nil) then // so right is not nil
+        get_min := min(get_min(root^.right),root^.value)
+    else if(root^.right = nil) then // so left is not nil
+        get_min := min(get_min(root^.left),root^.value)
+    else // both are not nil
+        get_min := min(min(get_min(root^.left),root^.value),get_min(root^.right));
+	
+end;
+
+function is_binary_search_tree(r : ptrTree) : boolean;
+begin
+	
+	is_binary_search_tree := (get_max(r^.left) < r^.value) and (get_min(r^.right) >= r^.value) ;
+
+	
+end;
+
 var arr : array [0..99] of integer;
-arrLength : integer;
+arrLength:integer;
 root : ptrTree;
 begin
 	
@@ -95,7 +141,17 @@ arr[11]:= 32;
     
     from_arr_to_binary_tree(root,arr,arrLength);
     Print_Pre_Fixe(root);
-    
+    writeln;
+    writeln('Sorted ? : ' ,is_binary_search_tree(root));
+    writeln;
+    writeln('Changing 66 to 65 : ');
+	root^.right^.left^.value := 65;
+	 writeln('Sorted ? : ' ,is_binary_search_tree(root));
+	 
+	 writeln;
+    writeln('Changing 65 to 45 : ');
+	root^.right^.left^.value := 45;
+	 writeln('Sorted ? : ' ,is_binary_search_tree(root));
 	
 end.
 
